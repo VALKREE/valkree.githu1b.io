1bf8af7709981e6820ae983a51af6e5613dfa5af
VALKREE <49433750+VALKREE@users.noreply.github.com>
2020-10-22 14:23:33 +0400

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f915517b40b56001e9518a0",
+				"5f915517b40b56001e9518a2"
+			]
+		},
+		"5f915517b40b56001e9518a0": {
+			"id": "5f915517b40b56001e9518a0",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f915517b40b56001e9518a2": {
+			"id": "5f915517b40b56001e9518a2",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
